6b5f4ea4aa05e547bb8ae7d51cfb1c27e25aab58
Mafudjebajau <mafudjibajau@gmail.com>
2025-11-06 15:13:55 +0000

    correcao teste de imagem de loading banner

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,11 +1,12 @@
 import './Loading.css'
+import Logo from '../../assets/logo.svg'
 
 
 const Loading = () => {
   return (
     <div className="loading">
       <div className="loading-spinner">
-        <img src="/src/assets/logo.svg" alt="itinterprises" style={{borderRadius:"50%", width:"800px",height:"800px",objectFit:"contain" }} />
+        <img src={Logo} alt="itinterprises" style={{borderRadius:"50%", width:"800px",height:"800px",objectFit:"contain" }} />
         <div className="spinner"></div>
       </div>
     </div>
